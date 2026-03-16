539b87e8d68eebd08fb145a299436de2fdc7207b
Prerna Arote <arote.prerna@gmail.com>
2026-03-16 09:02:14 +0100

    Update main.go

--- cmd/buyer/main.go
+++ cmd/buyer/main.go
@@ -7,33 +7,27 @@ import (
 	"net/http"
 	"time"
 
-	"qpay/pkg/types"
+	"qpay-artifact/pkg/types"
 )
 
 func main() {
 
 	tx := types.QPayTransaction{
-		TxID:    fmt.Sprintf("tx-%d", time.Now().UnixNano()),
-		Buyer:   "Alice",
-		Seller:  "Bob",
-		Amount:  0.001,
-		Message: "Q_pay micropayment",
+		TxID:   fmt.Sprintf("%d", time.Now().UnixNano()),
+		Buyer:  "Alice",
+		Seller: "Bob",
+		Amount: 0.001,
 	}
 
 	data, _ := json.Marshal(tx)
 
-	resp, err := http.Post(
-		"http://localhost:9000/request_payment",
+	start := time.Now()
+
+	http.Post("http://localhost:9000/request",
 		"application/json",
-		bytes.NewBuffer(data),
-	)
+		bytes.NewBuffer(data))
 
-	if err != nil {
-		fmt.Println("Error sending request:", err)
-		return
-	}
+	latency := time.Since(start)
 
-	defer resp.Body.Close()
-
-	fmt.Println("Buyer received response:", resp.Status)
+	fmt.Println("Buyer service latency:", latency)
 }
